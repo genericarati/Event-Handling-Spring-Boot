120edca65df9336b5306fa4f603e5d1e80b67731
banana: :dragon <gotdteam+banana-dragon@gmail.com>
2018-02-16 14:12:02 -0500

    Order object is returned

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,18 +2,21 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@CrossOrigin
 public class HelloController {
     
 	@Autowired
 	EventCaller eventCaller;
 	
     @RequestMapping("/")
-    public String index() {
+    
+    public Order index() {
     	    eventCaller.callFromMain();
-        return "Greetings from Spring Boot!";
+        return new Order ("12345", "dealer1");
     }
     
 }
---
+++ src/main/java/hello/Order.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Order {
+
+	public String ordernumber;
+	public String dealer;
+
+	public Order(String ordernumber, String dealer) {
+		this.ordernumber = ordernumber;
+
+		this.dealer = dealer;
+	}
+}
