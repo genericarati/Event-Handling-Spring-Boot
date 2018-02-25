ee4f4e2635515b6f1099e6201e5bc69362e029b4
genericarati <genericarati@gmail.com>
2018-02-24 22:12:15 -0500

    Added crossorigins support for websocket

--- src/main/java/com/somecompany/trading/WebSocketConfig.java
+++ src/main/java/com/somecompany/trading/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.somecompany.trading;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,15 +11,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
-    }
+	@Override
+	@CrossOrigin
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/gs-guide-websocket")
+		.setAllowedOrigins("http://localhost:3000")
+		.withSockJS();
+	}
 
 }
